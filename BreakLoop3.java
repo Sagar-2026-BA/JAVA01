a7e95c53e7ec97158dad5eaa8ec1387fa9776a0a
B A SAGAR PATEL <basagarpatel457@gmail.com>
2026-08-17 18:50:50 +0530

    Initial commit - Java Practice Programs

---
+++ BreakLoop3.java
@@ -0,0 +1,13 @@
+class BreakLoop3{
+    public static void main(String args[]){
+        for(int i=0;i<3;i++){
+            System.out.println("Pass"+i+":");
+            for(int j=0;j<100;j++){
+                if(j<=10)
+                System.out.println(j);
+            }
+            System.out.println();
+        }
+        System.out.println("Loops complete.");
+    }
+}
