f5ad10a8ea744dff99f55cfff09f146fc52e0bf5
B A SAGAR PATEL <basagarpatel457@gmail.com>
2026-09-14 12:19:36 +0530

    Add files via upload

---
+++ Pattern01.java
@@ -0,0 +1,18 @@
+public class Pattern01{
+    public static void main(String args[]){
+        
+        for(int i=0;i<=3;i++){
+            if(i==0 || i==3){
+                System.out.println("* * * *");
+                    }
+            else if(i==1){
+                System.out.println("*   * *");
+                        }
+            else if(i==2){
+                System.out.println("* *   *");
+            }
+            
+            }
+        }
+    
+}
