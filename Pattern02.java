bb8acb9c5139bfc8d167c90764918fb88731343c
B A SAGAR PATEL <basagarpatel457@gmail.com>
2026-09-14 12:19:53 +0530

    Add files via upload

---
+++ Pattern02.java
@@ -0,0 +1,18 @@
+public class Pattern02{
+    public static void main(String args[]){
+        for(int i=0;i<=4;i++){
+            if(i==0 || i==4){
+                System.out.println("* * * * *");
+                    }
+            else if(i==1){
+                System.out.println("* *     *");
+                    }
+            else if(i==2){
+                System.out.println("*   *   *");
+                    }
+            else if(i==3){
+                System.out.println("*     * *");
+                    }
+            }
+        }
+    }
